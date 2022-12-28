e6d6fb697b93cc46aee76da172b1b2c61f18f156
Punvireakroth <vireakrothpun@gmail.com>
2022-12-28 22:32:15 +0700

    apply inline style

--- enjoy_react/src/index.js
+++ enjoy_react/src/index.js
@@ -1,17 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-let img = "https://picsum.photos/200";
+const customStyle = {
+  color: "red",
+  fontSize: "20px",
+  border: "1px solid black"
+};
+
+customStyle.color = "pink";
+
 ReactDOM.render(
   <div>
-    <h1 className="heading" contentEditable="true" spellCheck="false">
-      Random Images
-    </h1>
-    <div className="container">
-      <img src={img} alt="random-img" />
-      <img src={img} alt="random-img" />
-      <img src={img} alt="random-img" />
-    </div>
+    <h1 style={customStyle}>Hello World!</h1>
   </div>,
   document.getElementById("root")
 );
